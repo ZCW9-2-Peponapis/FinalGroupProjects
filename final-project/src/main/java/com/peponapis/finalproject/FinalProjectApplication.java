87ac6dcc32b1c28ea9acc3fbd07281e55e5cd341
Christy <christy@Zipcoders-MacBook-Pro-90.local>
2024-01-03 17:25:04 -0500

    working in the FolderService,FolderRepo,

--- final-project/src/main/java/com/peponapis/finalproject/FinalProjectApplication.java
+++ final-project/src/main/java/com/peponapis/finalproject/FinalProjectApplication.java
@@ -1,7 +1,10 @@
 package com.peponapis.finalproject;
 
+import com.peponapis.finalproject.model.Folder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class FinalProjectApplication {
--- final-project/src/main/java/com/peponapis/finalproject/service/FolderService.java
+++ final-project/src/main/java/com/peponapis/finalproject/service/FolderService.java
@@ -1,4 +1,20 @@
 package com.peponapis.finalproject.service;
 
+import com.peponapis.finalproject.model.Folder;
+
 public class FolderService {
+    public void getDocsInFolder(int folderId){
+
+    }
+    public void saveFolder(Folder folder){
+
+    }
+
+    public void deleteFolder(int FolderId){
+
+    }
+
+    public void updateFolder(Folder folder){
+
+    }
 }
