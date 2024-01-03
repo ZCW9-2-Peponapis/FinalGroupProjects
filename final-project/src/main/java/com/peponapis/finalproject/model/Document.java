c69f7d86d6f9068850d6f2c2f53f41916f206e2d
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2024-01-03 11:48:32 -0500

    document model

--- final-project/src/main/java/com/peponapis/finalproject/model/Document.java
+++ final-project/src/main/java/com/peponapis/finalproject/model/Document.java
@@ -1,4 +1,77 @@
 package com.peponapis.finalproject.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String title;
+    String body;
+    Date creationDate;
+    Date modificationDate;
+    int userId; // id of user who created the document
+
+    public Document(String title, String body, int userId){
+        this.title = title;
+        this.body = body;
+        this.creationDate = new Date();
+        this.modificationDate = new Date();
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
