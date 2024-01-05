c85ab0fe56b0f8e236e05840fd22eb70d6cd52fc
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2024-01-05 10:08:48 -0500

    document service tests

--- final-project/src/test/java/com/peponapis/finalproject/FinalProjectApplicationTests.java
+++ final-project/src/test/java/com/peponapis/finalproject/FinalProjectApplicationTests.java
@@ -1,7 +1,12 @@
 package com.peponapis.finalproject;
 
+import com.peponapis.finalproject.repository.DocumentRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class FinalProjectApplicationTests {
@@ -9,5 +14,6 @@ class FinalProjectApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
 
 }
