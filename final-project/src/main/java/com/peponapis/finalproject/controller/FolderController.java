6dd6ebdbc460674bad3e569dc654eb7cc80de859
Christy <christy@Zipcoders-MacBook-Pro-90.local>
2024-01-05 15:14:57 -0500

    working in Controller

--- final-project/src/main/java/com/peponapis/finalproject/controller/FolderController.java
+++ final-project/src/main/java/com/peponapis/finalproject/controller/FolderController.java
@@ -1,4 +1,42 @@
 package com.peponapis.finalproject.controller;
 
+import com.peponapis.finalproject.model.Document;
+import com.peponapis.finalproject.model.Folder;
+import com.peponapis.finalproject.service.FolderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class FolderController {
+
+    FolderService folderService;
+@Autowired
+    public FolderController (FolderService folderService){
+          this.folderService = folderService;
+    }
+
+
+@GetMapping
+    public List<Document> viewDocs(int folderId){
+       return this.folderService.getDocsInFolder(folderId);
+    }
+@GetMapping
+    public Folder createFolder(Folder folder){
+      return this.folderService.saveFolder(folder) ;
+    }
+@GetMapping
+    public void deleteFolder(int folderId){
+      folderService.deleteFolder(folderId);
+    }
+
+@GetMapping
+    public Folder updateFolder(Folder folder){
+     return folderService.saveFolder(folder);
+    }
 }
--- final-project/src/main/java/com/peponapis/finalproject/service/FolderService.java
+++ final-project/src/main/java/com/peponapis/finalproject/service/FolderService.java
@@ -1,20 +1,35 @@
 package com.peponapis.finalproject.service;
 
+import com.peponapis.finalproject.model.Document;
 import com.peponapis.finalproject.model.Folder;
+import com.peponapis.finalproject.repository.FolderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class FolderService {
-    public void getDocsInFolder(int folderId){
 
+    FolderRepo folderRepo;
+    @Autowired
+     public FolderService(FolderRepo folderRepo){
+         this.folderRepo = folderRepo;
+     }
+
+
+    public List<Document> getDocsInFolder(int folderId){
+       return folderRepo.findById(folderId).get().getDocs();
     }
-    public void saveFolder(Folder folder){
-
+    public Folder saveFolder(Folder folder){
+      return folderRepo.save(folder);
     }
 
     public void deleteFolder(int FolderId){
-
+         folderRepo.deleteById(FolderId);
     }
 
-    public void updateFolder(Folder folder){
-
+    public Folder updateFolder(Folder folder){
+         return folderRepo.save(folder);
     }
 }
