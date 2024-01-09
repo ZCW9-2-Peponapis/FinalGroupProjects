44db582af953d708d1228bd5c54a994f6255ea3a
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2024-01-08 19:59:25 -0500

    updating user and doc service to reflect auth refactoring

--- final-project/src/main/java/com/peponapis/finalproject/service/DocumentService.java
+++ final-project/src/main/java/com/peponapis/finalproject/service/DocumentService.java
@@ -1,12 +1,20 @@
 package com.peponapis.finalproject.service;
 
+import com.peponapis.finalproject.dtos.DocumentDTO;
 import com.peponapis.finalproject.model.Document;
-import com.peponapis.finalproject.model.User;
+import com.peponapis.finalproject.model.UserEntity;
 import com.peponapis.finalproject.repository.DocumentRepo;
+import com.peponapis.finalproject.repository.UserRepo;
+import com.peponapis.finalproject.security.AuthenticationFacade;
+import com.peponapis.finalproject.security.IAuthenticationFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.print.Doc;
+import java.security.Principal;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -15,19 +23,49 @@ import java.util.List;
 @Service
 public class DocumentService {
     DocumentRepo documentRepo;
+    UserRepo userRepo;
+    private IAuthenticationFacade authenticationFacade;
 
     @Autowired
-    public DocumentService(DocumentRepo documentRepo){
+    public DocumentService(DocumentRepo documentRepo, AuthenticationFacade authenticationFacade, UserRepo userRepo){
         this.documentRepo = documentRepo;
+        this.authenticationFacade = authenticationFacade;
+        this.userRepo = userRepo;
     }
 
     /**
      *
-     * @param document document to update
-     * @return the new document
+     * @param document document to create
+     * @return DTO of the new document
      */
-    public Document saveDocument(Document document){
-        return this.documentRepo.save(document);
+    public DocumentDTO createDocument(Document document){
+        Authentication authentication = authenticationFacade.getAuthentication();
+        //LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) authentication.getDetails();
+        User user = (User) authentication.getPrincipal();
+
+        UserEntity docAuthor = userRepo.findByUserName(user.getUsername()).get();
+        this.documentRepo.save(document);
+        docAuthor.addDocument(document);
+        document.setUser(docAuthor);
+        this.userRepo.save(docAuthor);
+
+        DocumentDTO doc = new DocumentDTO();
+        doc.setAuthor(document.getUser().getName());
+        doc.setTitle(document.getTitle());
+        doc.setBody(document.getBody());
+        doc.setCreationDate(document.getCreationDate());
+        doc.setModificationDate(document.getModificationDate());
+
+        return doc;
+    }
+
+    /**
+     *
+     * @param document document to be updated
+     * @return DTO of the updated document
+     */
+    public DocumentDTO updateDocument(Document document){
+        return new DocumentDTO(this.documentRepo.save(document));
     }
 
     /**
@@ -49,11 +87,6 @@ public class DocumentService {
         }
         return null;
     }
-
-//    Probably don't need this, it's the same as saveDocument
-//    public Document createDocument(Document document){
-//        return this.documentRepo.save(document);
-//    }
 
     /**
      *
--- final-project/src/main/java/com/peponapis/finalproject/service/UserService.java
+++ final-project/src/main/java/com/peponapis/finalproject/service/UserService.java
@@ -1,7 +1,7 @@
 package com.peponapis.finalproject.service;
 
 
-import com.peponapis.finalproject.model.User;
+import com.peponapis.finalproject.model.UserEntity;
 import com.peponapis.finalproject.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,24 +30,25 @@ public class UserService {
     }
 
     /**
-     * @param user to create or update
+     * @param userEntity to create or update
      * @return the new user into the user repo
      * if the username already exists, unable to create user
      */
-    public User saveUser(User user){
-        if (userRepo.existsByUserName(user.getUserName())){
+    public UserEntity saveUser(UserEntity userEntity){
+        if (userRepo.existsByUserName(userEntity.getUserName())){
             return null;
         }
-        String password = passwordEncoder.encode((user.getPassword()));
-        user.setPassword(password);
-        return userRepo.save(user);
+        String password = passwordEncoder.encode((userEntity.getPassword()));
+        userEntity.setPassword(password);
+
+        return userRepo.save(userEntity);
     }
 
     /**
      * @param id for the user
      * @return the user with given id or return null
      */
-    public User getUser(int id){
+    public UserEntity getUser(int id){
         if(userRepo.findById(id).isPresent()){
             return userRepo.findById(id).get();
         }
@@ -59,7 +60,7 @@ public class UserService {
      * @param name of the user
      * @return the user with given name that exists in user repo
      */
-    public User findByName(String name){
+    public UserEntity findByName(String name){
         return userRepo.findByName(name);
     }
 
@@ -68,7 +69,7 @@ public class UserService {
      * @param username of the user
      * @return the username with given username that exists in user repo
      */
-    public Optional<User> findByUserName(String username){
+    public Optional<UserEntity> findByUserName(String username){
         return userRepo.findByUserName(username);
     }
 
@@ -78,7 +79,7 @@ public class UserService {
      * user password is encrypted, must encrypt the password before searching,
      * @return the password that is given that exists in user repo
      */
-    public User findByPassword(String password) {
+    public UserEntity findByPassword(String password) {
         String encryptPass = passwordEncoder.encode(password);
         return userRepo.findByPassword(encryptPass);
     }
@@ -92,12 +93,14 @@ public class UserService {
      *                                  for username and user password
      */
 
-    public User authenicatorUser (String username, String password) throws AuthenticationException {
-        User user = userRepo.findByUserName(username)
+    public UserEntity authenicatorUser (String username, String password) throws AuthenticationException {
+        UserEntity userEntity = userRepo.findByUserName(username)
                 .orElseThrow(() -> new AuthenticationException("Username doesn't exist"));
-        if (!passwordEncoder.matches(password, user.getPassword())){
+        if (!passwordEncoder.matches(password, userEntity.getPassword())){
             throw new AuthenticationException("Invalid Login");
         }
-        return user;
+        return userEntity;
     }
+
+
 }
