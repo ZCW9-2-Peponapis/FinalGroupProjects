b433325cf73c769cea3f38d9a031d14bce3b0848
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2024-01-10 17:27:27 -0500

    rebasing

--- final-project/frontendj/src/components/TextEditor.js
+++ final-project/frontendj/src/components/TextEditor.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Import the styles
 
 const TextEditor = () => {
     const [content, setContent] = useState('');
-    const [readOnly, setReadOnly] = useState(false); // State to manage read-only mode
+    const [document, setDocument] = useState('');
+
+    // fetching document from backend
+    useEffect(() => {
+        fetch('http://localhost:8080/document/view/253', {
+            method: 'GET',
+        }).then((res) => {
+            return res.json();
+        }).then((data) => {
+            setContent(data.body);
+            setDocument(data);
+        });
+    }, []);
 
     const handleChange = (value) => {
         setContent(value);
     };
 
+    // resources: maybe will need this later when saving updates
+    // https://jasonwatmore.com/post/2020/02/01/react-fetch-http-post-request-examples
     const handleSave = () => {
         // Your save logic here
     };
@@ -47,10 +61,10 @@ const TextEditor = () => {
     return (
         <div>
             <h1>Text Editor</h1>
+            <h2>{document.title}</h2>
             <ReactQuill
                 value={content}
                 onChange={handleChange}
-                readOnly={readOnly} // Set readOnly conditionally
                 modules={modules}
                 formats={formats}
                 style={{ height: '900px', width: '1000px', ...styles }}
