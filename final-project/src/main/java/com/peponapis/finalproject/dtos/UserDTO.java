baf575ac0617b39b69085faa63730142f0617e8b
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2024-01-11 20:12:29 -0500

    added token field into user dto to pass to frontend

--- final-project/src/main/java/com/peponapis/finalproject/dtos/UserDTO.java
+++ final-project/src/main/java/com/peponapis/finalproject/dtos/UserDTO.java
@@ -5,10 +5,14 @@ import com.peponapis.finalproject.model.UserEntity;
 public class UserDTO {
     int id;
     String name;
+    private String accessToken;
+    private String tokenType = "Bearer ";
 
-    public UserDTO(UserEntity user){
+    public UserDTO(UserEntity user, String token){
         this.id = user.getUserId();
         this.name = user.getName();
+        System.out.println(token);
+        this.accessToken = token;
     }
 
     public int getId() {
@@ -25,5 +29,13 @@ public class UserDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 }
