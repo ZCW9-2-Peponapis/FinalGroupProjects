211f7708830438021af0cf7d90b6986d44ca72e2
Christy <christy@Zipcoders-MacBook-Pro-90.local>
2024-01-12 11:16:55 -0500

    rebase

--- final-project/frontendj/src/components/Searchbar.js
+++ final-project/frontendj/src/components/Searchbar.js
@@ -1,15 +1,19 @@
 import "./Searchbar.css";
-
+import SearchIcon from '@mui/icons-material/Search';
 
 
 function SearchBar({placeholder, dataResult}) {
         return(
+        <div className="parent-container">
         <div className="search">
         <div className="searchInputs">
         <input  type="text" placeholder={placeholder} />
-        <div className="searchIcon"></div>
+        <div className="searchIcon">
+        <SearchIcon/>
+        </div>
         </div>
         <div className="dataResult"></div>
+        </div>
         </div>
 
         );
