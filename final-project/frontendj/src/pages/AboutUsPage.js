e93a0cd5c44ba66ace23b96daa760549f772ac52
hyunjialee <hyjialee@gmail.com>
2024-01-14 10:35:45 -0500

    add profiles images to about page

--- final-project/frontendj/src/pages/AboutUsPage.js
+++ final-project/frontendj/src/pages/AboutUsPage.js
@@ -2,27 +2,30 @@ import React from 'react';
 import './AboutUsPage.css';
 import userPic from '../user.png';
 import jia from '../profiles/hyunji.png'
+import tam from '../profiles/tam.png'
+import christy from '../profiles/christy.png'
 
 function AboutUsPage (){
 
 
     return (
         <div className="about-section">
-            <h1>About Us Page</h1>
-            <p>Thank you for your time. Hope you enjoy ZipDocs!</p>
-            <p>We would love to answer any questions.</p>
+            <h1>About Us</h1>
+            <h2>Thank you for your time. Hope you enjoy ZipDocs!</h2>
+            <h2>We would love to answer any questions.</h2>
 
             <h2 style={{ textAlign: 'center' }}>Our Team</h2>
 
             <div className="row">
                 <div className="column">
                     <div className="card">
-                        <img src={userPic} alt="Tam" style={{ width: '100%' }} />
+                        <img src={tam} alt="Tam" style={{ width: '100%' }} />
                         <div className="container">
                             <h2>Tam Maria Doan</h2>
                             <p className="title">Software Developer</p>
-                            <p>Description.</p>
-                            <p>email@example.com</p>
+                            <p>B.S in Computer Science</p>
+                            <p>Former Passenger Service Agent with Sun Country Airlines</p>
+                            <p>mdtam147@gmail.com</p>
                             <p>
                                 <a href='https://github.com/TamMDoan'
                                    target="_blank"
@@ -45,11 +48,12 @@ function AboutUsPage (){
 
                 <div className="column">
                     <div className="card">
-                        <img src={userPic} alt="Christy" style={{width: '100%'}}/>
+                        <img src={christy} alt="Christy" style={{width: '100%'}}/>
                         <div className="container">
                             <h2>Christianne Edgard</h2>
                             <p className="title">Software Developer</p>
-                            <p>Description.</p>
+                            <p>US Army Veteran</p>
+                            <p>Serving in the PA Army National Guard</p>
                             <p>Edgardchristianne@gmail.com</p>
                             <p>
                                 <a href='https://github.com/Christy-ED'
@@ -78,6 +82,7 @@ function AboutUsPage (){
                             <h2>Hyunji Lee</h2>
                             <p className="title">Software Developer</p>
                             <p>B.A in Psychology</p>
+                            <p>Former emergency medical scribe and assistant manager</p>
                             <p>hyjialee@gmail.com</p>
                             <p>
                                 <a href='https://github.com/hyunjialee/'
