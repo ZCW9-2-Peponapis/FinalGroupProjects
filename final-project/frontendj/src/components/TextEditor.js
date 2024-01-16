58d69721ec8a1d16c1ee4c38c6cf1aca31c7f2a9
hyunjialee <hyjialee@gmail.com>
2024-01-16 17:30:51 -0500

    styling BIGGER FONT per Kris

--- final-project/frontendj/src/components/TextEditor.js
+++ final-project/frontendj/src/components/TextEditor.js
@@ -129,8 +129,7 @@ const TextEditor = (id) => {
             position: 'absolute', left: '50%', top: '25%',
             transform: 'translate(-50%, -5%)'
         }}>
-            <h1>Text Editor</h1>
-            <h2>{title}</h2>
+            <h1>{title}</h1>
             <button id="save-btn" onClick={handleSave}>Save</button>
             <button id="save-btn" onClick={handleExportPDF}>Export as PDF</button>
             <button id="save-btn" onClick={handleClear}>Clear</button>
--- final-project/frontendj/src/pages/AboutUsPage.js
+++ final-project/frontendj/src/pages/AboutUsPage.js
@@ -14,12 +14,8 @@ function AboutUsPage (){
 
     return (
         <div className="about-section">
-            <h1 style={{fontSize: '80px', marginTop: '-15px'}}>About Us</h1>
-            <h2>Thank you for your time. Hope you enjoy ZipDocs!</h2>
-            <h2>We would love to answer any questions.</h2>
-
-            <h1 style={{textAlign: 'center'}}>Our Team</h1>
-            <h2 style={{marginBottom: '-30px'}}>We would love to connect through our LinkedIn QR Codes.</h2>
+            <h1 style={{fontSize: '25px', marginTop: '-15px'}}>Our Team</h1>
+            <h2 style={{marginBottom: '-30px'}}>Connect through our LinkedIn QR Codes.</h2>
 
             <div className="row">
                 <div className="column">
@@ -27,20 +23,7 @@ function AboutUsPage (){
                         <img src={tam} alt="Tam" style={{width: '100%'}}/>
                         <div className="container">
                             <h2>Tam Maria Doan</h2>
-                            <p className="title">Software Developer</p>
-                            <p>B.S. in Computer Science</p>
-                            <p>Former Passenger Service Agent with Sun Country Airlines</p>
-                            <p>mdtam147@gmail.com</p>
                             <img src={tamqr} alt="TamQR" style={{width: '80%'}}/>
-
-                            <p>
-                                <a href='https://github.com/TamMDoan'
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-
-                                    <button className="button">GitHub</button>
-                                </a>
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -50,20 +33,7 @@ function AboutUsPage (){
                         <img src={christy} alt="Christy" style={{width: '100%'}}/>
                         <div className="container">
                             <h2>Christianne (Christy) Edgard</h2>
-                            <p className="title">Software Developer</p>
-                            <p>US Army Veteran</p>
-                            <p>Serving in the PA Army National Guard</p>
-                            <p>Edgardchristianne@gmail.com</p>
                             <img src={christyqr} alt="christyqr" style={{width: '80%'}}/>
-
-                            <p>
-                                <a href='https://github.com/Christy-ED'
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-
-                                    <button className="button">GitHub</button>
-                                </a>
-                            </p>
 
                         </div>
                     </div>
@@ -74,19 +44,7 @@ function AboutUsPage (){
                         <img src={jia} alt="Jia" style={{width: '100%'}}/>
                         <div className="container">
                             <h2>Hyunji (Jia) Lee</h2>
-                            <p className="title">Software Developer</p>
-                            <p>B.A. in Psychology</p>
-                            <p>Former Emergency Medical Scribe & Assistant Manager </p>
-                            <p>hyjialee@gmail.com</p>
                             <img src={jiaqr} alt="JiaQR" style={{width: '80%'}}/>
-                            <p>
-                                <a href='https://github.com/hyunjialee/'
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-
-                                    <button className="button">GitHub</button>
-                                </a>
-                            </p>
                         </div>
                     </div>
                 </div>
