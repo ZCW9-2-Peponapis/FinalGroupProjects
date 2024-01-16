a8bfedcd3c570506354fd51ce337a91e05ce8556
hyunjialee <hyjialee@gmail.com>
2024-01-15 21:59:57 -0500

    styling and added log out feature to the navi bar

--- final-project/frontendj/src/components/NaviBar.js
+++ final-project/frontendj/src/components/NaviBar.js
@@ -7,14 +7,31 @@ function NaviBar() {
         // Check user login status here, login and register does not appear
         const userLoggedIn = localStorage.getItem('userInfo') !== null;
         setIsLoggedIn(userLoggedIn);
+
     }, []);
+
+
+    const handleLogout = () => {
+        // Perform logout actions, e.g., clearing local storage
+        localStorage.removeItem('userInfo');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userId');
+        // Update the state to reflect the logout
+        setIsLoggedIn(false);
+        // Add any additional logout actions here
+
+        // Redirect to the home page or perform any other necessary actions
+        window.location.href = '/';
+    };
+
 
 
     return(
         <div className="navbar">
             <a href="/">Home</a>
-            {isLoggedIn ? false : <a href="/login">Login</a>}
-            {isLoggedIn ? false : <a href="/register">Register</a>}
+            {!isLoggedIn && <a href="/login">Login</a>}
+            {!isLoggedIn && <a href="/register">Register</a>}
+            {isLoggedIn && <a onClick={handleLogout}>Logout</a>}
             <div className="dropdown">
                 <button className="dropbtn">Info
                     <i className="fa fa-caret-down"></i>
--- final-project/frontendj/src/pages/AboutUsPage.js
+++ final-project/frontendj/src/pages/AboutUsPage.js
@@ -4,42 +4,41 @@ import userPic from '../user.png';
 import jia from '../profiles/hyunji.png'
 import tam from '../profiles/tam.png'
 import christy from '../profiles/christy.png'
+import jiaqr from '../profiles/jiaQR.JPG'
+import tamqr from '../profiles/tamqr.png'
+import christyqr from '../profiles/christyQR.png'
+
 
 function AboutUsPage (){
 
 
     return (
         <div className="about-section">
-            <h1>About Us</h1>
+            <h1 style={{fontSize: '80px', marginTop: '-15px'}}>About Us</h1>
             <h2>Thank you for your time. Hope you enjoy ZipDocs!</h2>
             <h2>We would love to answer any questions.</h2>
 
-            <h2 style={{ textAlign: 'center' }}>Our Team</h2>
+            <h1 style={{textAlign: 'center'}}>Our Team</h1>
+            <h2 style={{marginBottom: '-30px'}}>We would love to connect through our LinkedIn QR Codes.</h2>
 
             <div className="row">
                 <div className="column">
                     <div className="card">
-                        <img src={tam} alt="Tam" style={{ width: '100%' }} />
+                        <img src={tam} alt="Tam" style={{width: '100%'}}/>
                         <div className="container">
                             <h2>Tam Maria Doan</h2>
                             <p className="title">Software Developer</p>
-                            <p>B.S in Computer Science</p>
+                            <p>B.S. in Computer Science</p>
                             <p>Former Passenger Service Agent with Sun Country Airlines</p>
                             <p>mdtam147@gmail.com</p>
+                            <img src={tamqr} alt="TamQR" style={{width: '80%'}}/>
+
                             <p>
                                 <a href='https://github.com/TamMDoan'
                                    target="_blank"
                                    rel="noopener noreferrer">
 
                                     <button className="button">GitHub</button>
-                                </a>
-                            </p>
-                            <p>
-                                <a href='https://www.linkedin.com/in/tammariadoan/'
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-
-                                    <button className="button">LinkedIn</button>
                                 </a>
                             </p>
                         </div>
@@ -55,6 +54,8 @@ function AboutUsPage (){
                             <p>US Army Veteran</p>
                             <p>Serving in the PA Army National Guard</p>
                             <p>Edgardchristianne@gmail.com</p>
+                            <img src={christyqr} alt="christyqr" style={{width: '80%'}}/>
+
                             <p>
                                 <a href='https://github.com/Christy-ED'
                                    target="_blank"
@@ -63,14 +64,7 @@ function AboutUsPage (){
                                     <button className="button">GitHub</button>
                                 </a>
                             </p>
-                            <p>
-                                <a href='https://www.linkedin.com/in/christianne-edgard/'
-                                   target="_blank"
-                                   rel="noopener noreferrer">
 
-                                    <button className="button">LinkedIn</button>
-                                </a>
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -81,23 +75,16 @@ function AboutUsPage (){
                         <div className="container">
                             <h2>Hyunji (Jia) Lee</h2>
                             <p className="title">Software Developer</p>
-                            <p>B.A in Psychology</p>
+                            <p>B.A. in Psychology</p>
                             <p>Former Emergency Medical Scribe & Assistant Manager </p>
                             <p>hyjialee@gmail.com</p>
+                            <img src={jiaqr} alt="JiaQR" style={{width: '80%'}}/>
                             <p>
                                 <a href='https://github.com/hyunjialee/'
                                    target="_blank"
                                    rel="noopener noreferrer">
 
                                     <button className="button">GitHub</button>
-                                </a>
-                            </p>
-                            <p>
-                                <a href='https://www.linkedin.com/in/hyunjialee/'
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-
-                                    <button className="button">LinkedIn</button>
                                 </a>
                             </p>
                         </div>
